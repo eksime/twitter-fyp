571b03563e35a021dc393bfee0e3831c78d1656a
Elliot Sime <e.sime@lancs.ac.uk>
2016-04-07 16:34:21 +0100

    Add comments

--- script.py
+++ script.py
@@ -11,8 +11,9 @@ from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
 from multiprocessing.dummy import Pool as ThreadPool
 
-limit = 10000
-cv = 10
+# "globals"
+limit = 10000 #sample size / 2
+cv = 10 #n-fold cv
 
 '''
 clf = Pipeline([('vect', TfidfVectorizer()),
@@ -20,6 +21,7 @@ clf = Pipeline([('vect', TfidfVectorizer()),
                ])
 '''
 
+# Create sklearn pipeline
 vect = TfidfVectorizer()
 sel = SelectFromModel(LinearSVC(loss='squared_hinge', penalty="l1", dual=False))
 rfc = RandomForestClassifier(n_estimators=100, n_jobs=-1)
@@ -29,6 +31,7 @@ clf = Pipeline([('vect', vect),
                 ('clf', rfc)
                ])
 
+# Sqlite3 Connection
 conn = sqlite3.connect('twitter.db')
 c = conn.cursor()
 d = conn.cursor()
@@ -37,15 +40,17 @@ limit = (limit, )
 def getFeatureVector(uid):
     con2 = sqlite3.connect('twitter.db')
     d = con2.cursor()
-    d.execute('select group_concat(text) from tweet, user where tweet.user = user.id and user.id = ?;', uid)
+    d.execute('select group_concat(text) from tweet, user where tweet.user = user.id and user.id = ?;', uid) #get all tweet text from a user
     return d.fetchone()[0]
 
+# Print bot / user totals
 c.execute('select count(*) from tweet, user where user.is_bot = 1 and tweet.user = user.id;')
 bot_count = c.fetchone()
 c.execute('select count(*) from tweet, user where user.is_bot = 0 and tweet.user = user.id;')
 user_count = c.fetchone()
 print('bots: ' + str(bot_count[0]) + ' users: ' + str(user_count[0]))
 
+# Positive sample processing
 print("Processing positive data: ", end="")
 pool = ThreadPool(4)
 pos_uids = c.execute('select id from user where user.is_bot = 1 order by random() limit ?;', limit)
@@ -55,6 +60,7 @@ pool.join()
 y_data_labels = np.ones(len(pos_data))
 print("Done")
 
+# Negative sample processing
 print("Processing negative data: ", end="")
 pool = ThreadPool(4)
 neg_uids = c.execute('select id from user where user.is_bot = 0 order by random() limit ?;', limit)
@@ -64,27 +70,33 @@ pool.join()
 y_data_labels = np.append(y_data_labels, np.zeros(len(neg_data)))
 print("Done")
 
+# Concatenate training data
 x_data_train = pos_data + neg_data
 
+# Fit + transform data
 print("Fitting model: ", end="")
 clf.fit_transform(x_data_train, y_data_labels)
 print("Done")
 
+# Calculate and print cv scores
 scores = cross_validation.cross_val_score(clf, x_data_train, y_data_labels, cv=cv, scoring='f1_weighted')
-
 print("scores: " + str(np.average(scores)))
 print(clf.predict([getFeatureVector((22933636,))]))
 
+# Feature importance
 importances = rfc.feature_importances_
 indices = np.argsort(importances)[::-1]
-
 feature_names = vect.get_feature_names()
 table = []
+
+# From what I understand feature_names[indices[f]] should be the feature name that corresponds to the feature index in indices[f]
 
 for f in range(len(importances)):
     table.append([feature_names[indices[f]], importances[indices[f]]])
 
+# Pretty-print a table.
 df = pd.DataFrame(table)
 print(df.to_string(header=False))
 
+# Enter interactive mode
 code.interact(local=locals())
